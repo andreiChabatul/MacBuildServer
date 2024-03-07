e1c16de1503460f220aca896e484986284c5628f
Andrei Chabatul <andreichabatul@gmail.com>
2024-03-07 10:42:49 +0300

    feat: install redux

---
+++ frontend/src/app/components/ui/forms/const/const.ts
@@ -0,0 +1,6 @@
+import { nameForm } from "@/types/formAuth";
+
+export const NAME_FORM: nameForm = {
+    login: 'Войти',
+    register : 'Зарегистрироваться'
+}
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Heebo } from "next/font/google";
 import "./globals.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import StoreProvider from "./StoreProvider";
 
 const font = Heebo({
   subsets: ["latin", "hebrew"],
@@ -21,12 +22,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>
-        <Header />
-        {children}
-        <Footer />
-      </body>
-    </html>
+    <StoreProvider>
+      <html lang="en">
+        <body className={font.className}>
+          <Header />
+          {children}
+          <Footer />
+        </body>
+      </html>
+    </StoreProvider>
   );
 }
