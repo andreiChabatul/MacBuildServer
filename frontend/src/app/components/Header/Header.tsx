aab5434657f5b3770c6a94554af590bb4371c72b
Andrei Chabatul <andreichabatul@gmail.com>
2024-03-07 10:42:39 +0300

    fix: add logo link header

--- frontend/src/app/components/Header/Header.tsx
+++ frontend/src/app/components/Header/Header.tsx
@@ -2,24 +2,22 @@
 
 import Image from "next/image";
 import styles from './header.module.scss';
-import { useShowBurger } from "./hooks/useShowBurger";
-
-import { useEffect, useState } from "react";
 import Menu from "./components/menu/Menu";
+import { useRouter } from "next/navigation";
+
 
 export default function Header() {
 
-
-    const { isShowBurger } = useShowBurger();
-    const [isOpen, setIsOpen] = useState(true);
-
-    useEffect(() => { setIsOpen(!isShowBurger) }, [isShowBurger])
-
+    const router = useRouter();
 
     return <header className={styles.wrapper}>
-        <Image className={styles.logo} src={'/logo.png'} width={45} height={45} alt="logo" />
-        
-        {/* {isOpen && <Menu />} */}
+        <Image
+            onClick={() => router.push('/')}
+            className={styles.logo}
+            src={'/logo.png'}
+            width={45}
+            height={45}
+            alt="logo" />
         <Menu />
     </header>
 }
