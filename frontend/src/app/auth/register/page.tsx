ea1cc5b6330ce65348e580efe73964ba1115563d
Andrei Chabatul <andreichabatul@gmail.com>
2024-03-07 10:23:07 +0300

    feat: add page register

---
+++ frontend/src/app/auth/register/page.tsx
@@ -0,0 +1,9 @@
+import FormAuth from "@/app/components/ui/forms/formAuth";
+import styles from "./register.module.scss";
+
+export default function PageRegister() {
+    return <div className={styles.wrapper}>
+        <h3 className={styles.title}>Регистрация нового пользователя</h3>
+        <FormAuth type='register' />
+    </div>
+}
